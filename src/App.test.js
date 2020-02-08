1d5a5db7a7888ef24206984c9d19c69a45a91fa1
Luke O'Malley <Lukeomalley95@gmail.com>
2020-02-08 17:00:02 -0500

    setup docker images

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,7 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('adds a number', () => {
+  expect(1 + 1).toBe(2);
+});
